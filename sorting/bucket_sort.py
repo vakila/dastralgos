30c2cd7644dd53d573959f6e2bec468434f54c1e
Anjana <anjanav@coli.uni-saarland.de>
2016-02-16 14:42:46 +0100

    Stub out bucket sort (not implemented yet)

---
+++ sorting/bucket_sort.py
@@ -0,0 +1,24 @@
+### Bucket sort in Python 3
+
+def bucket_sort(my_list):
+    '''Returns the sorted list (ascending) using this algorithm:
+    #TODO
+
+    >>> print(bucket_sort([4,2,1,3,5]))
+    [1, 2, 3, 4, 5]
+    >>> print(bucket_sort([2343, -43, 0, 1232, -586]))
+    [-586, -43, 0, 1232, 2343]
+    >>> print(bucket_sort([1,2,3]))
+    [1, 2, 3]
+    >>> print(bucket_sort([1]))
+    [1]
+    >>> print(bucket_sort([]))
+    []
+    '''
+    # TODO
+    return my_list
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
