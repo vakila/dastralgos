d21c2c03a3f6880c4770f62219b0c24abf6c01f9
Anjana <anjanav@coli.uni-saarland.de>
2016-02-16 15:51:46 +0100

    One possible solution to check_balanced

--- trees/4.4-check-balanced.py
+++ trees/4.4-check-balanced.py
@@ -1,3 +1,7 @@
+# Given a tree, write an algorithm to determine whether or not the tree is
+# (height-)balanced, i.e. whether the difference in depth between any two leaves
+# is less than or equal to 1.
+
 class BinaryTreeNode:
     def __init__(self, val):
         self.val = val
@@ -9,18 +13,37 @@ class BinaryTreeNode:
 
 
 
-def check_balanced_old(root_node, depth=0):
-    left_balanced, left_depth = check_balanced(root_node.left, depth = depth+1)
-    right_balanced, right_depth = check_balanced(root_node.right, depth = depth+1)
+def check_balanced_with_depth(root_node, depth=0):
+    if root_node.is_leaf():
+        return (True, depth)
 
-    if not (left_balanced and right_balanced):
-        return (False, max(left_depth, right_depth)+1))
+    children = {'left': root_node.left, 'right': root_node.right}
+    balanced = {'left': True, 'right': True}
+    depths = {'left': 0, 'right': 0}
+
+    for side in ["left", "right"]:
+        if children[side] != None:
+            balanced[side], depths[side] = check_balanced(children[side],
+                                                          depth = depth + 1)
+        # else (children[side] == None) that side is balanced with depth 0
+
+
+    tree_depth = max(depths.values()) + 1
+    if False in balanced.values():
+        return (False, tree_depth)
     else: #both are balanced, check depths
-        return abs(left_depth - right_depth) <= 1
+        depth_diff = max(depths.values()) - min(depths.values())
+        depth_diff = abs(depths['left'] - depths['right'])
+        return (depth_diff <= 1, tree_depth)
 
 
 def check_balanced(root_node):
+    return check_balanced_with_depth(root_node)[0]
+
+
+def check_balanced_alt(root_node):
     max_depth = 0
+
 
     # traverse the tree until hitting a leaf
     # compare its depth to max depth
